711234179cb1e177f7de196dc88d60a3c3836ae9
Kuba <kubaz9991@gmail.com>
2020-01-11 17:50:55 +0100

    Add buttons and svg

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -1,20 +1,18 @@
 import styled from "styled-components"
 
-const Button = styled.button`
-  width: 200px;
-  height: 40px;
+const ButtonLink = styled.a`
   margin: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
   background-color: #00499b;
   box-shadow: 0 10px 40px -15px black;
   color: white;
   text-decoration: none;
+  border: 3px solid orange;
+  padding: 10px;
   &:hover {
     cursor: pointer;
   }
+  display: flex;
+  justify-content: center;
 `
 
-export default Button
+export default ButtonLink

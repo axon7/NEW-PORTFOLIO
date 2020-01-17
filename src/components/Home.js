feeb54743ea918455280a1e080e78d358c62e56f
Kuba <kubaz9991@gmail.com>
2020-01-17 15:54:02 +0100

    First page as Home component

---
+++ src/components/Home.js
@@ -0,0 +1,93 @@
+import React from "react"
+import styled from "styled-components"
+import SEO from "./seo"
+import Button from "./Button"
+import lightning from "../images/lightning.svg"
+import { Link, animateScroll as scroll } from "react-scroll"
+
+const StyledMain = styled.main`
+  width: 100%;
+  background-color: #ff9e2c;
+  height: 100%;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  text-align: left;
+  transform: skew(0deg, -8deg);
+  margin-top: -150px;
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+  transform: skew(0deg, 8deg);
+  padding-top: 100px;
+  padding-bottom: 100px;
+`
+const Text = styled.div`
+  text-align: left;
+  width: 69%;
+  margin-bottom: 50px;
+  margin-top: 50px;
+`
+
+const Intro = styled.p`
+  width: 63%;
+  line-height: 1.5em;
+`
+const Lightning = styled.img`
+  margin-top: 50px;
+  display: flex;
+  align-self: flex-end;
+`
+
+const StyledH1 = styled.h1`
+  font-weight: 900;
+  font-size: 2.6em;
+  word-break: keep-all;
+  margin-bottom: 0px;
+  text-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
+`
+
+const DevTitle = styled.p`
+  color: white;
+  font-weight: 600;
+  margin-bottom: 18px;
+  margin-top: -2px;
+`
+const Home = () => {
+  return (
+    <>
+      <SEO title="home" />
+      <StyledMain id="home">
+        <Content>
+          <Lightning src={lightning} alt="lightning svg" />
+          <Text>
+            <StyledH1>Kuba Zając</StyledH1>
+            <DevTitle>WEB DEVELOPER</DevTitle>
+            <Intro>
+              I’m a self-taught front-end developer focused on creating{" "}
+              <b>clean</b>, <i>blazing fast</i> apps.
+            </Intro>
+          </Text>
+          <Button
+            as={Link}
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+          >
+            SHOW PROJECTS
+          </Button>
+        </Content>
+      </StyledMain>
+    </>
+  )
+}
+
+export default Home
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,88 +1,13 @@
 import React from "react"
-import styled from "styled-components"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Button from "../components/Button"
-import lightning from "../images/lightning.svg"
-import { Link } from "gatsby"
+import Home from "../components/Home"
 import ProjectList from "../components/ProjectList"
 import Technologies from "../components/Technologies"
 import Contact from "../components/Contact"
 
-const StyledMain = styled.main`
-  width: 100%;
-  background-color: #ff9e2c;
-  height: 100%;
-  color: black;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-items: center;
-  text-align: left;
-  transform: skew(0deg, -8deg);
-  margin-top: -150px;
-`
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-items: center;
-  transform: skew(0deg, 8deg);
-  padding-top: 100px;
-  padding-bottom: 100px;
-`
-const Text = styled.div`
-  text-align: left;
-  width: 69%;
-  margin-bottom: 50px;
-  margin-top: 50px;
-`
-
-const Intro = styled.p`
-  width: 63%;
-  line-height: 1.5em;
-`
-const Lightning = styled.img`
-  margin-top: 50px;
-  display: flex;
-  align-self: flex-end;
-`
-
-const StyledH1 = styled.h1`
-  font-weight: 900;
-  font-size: 2.6em;
-  word-break: keep-all;
-  margin-bottom: 0px;
-  text-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
-`
-
-const DevTitle = styled.p`
-  color: white;
-  font-weight: 600;
-  margin-bottom: 18px;
-  margin-top: -5px;
-`
-
 const IndexPage = () => (
   <Layout>
-    <SEO title="home" />
-    <StyledMain>
-      <Content>
-        <Lightning src={lightning} alt="lightning svg" />
-        <Text>
-          <StyledH1>Kuba Zając</StyledH1>
-          <DevTitle>WEB DEVELOPER</DevTitle>
-          <Intro>
-            I’m a self-taught front-end developer focused on creating{" "}
-            <b>clean</b>, <i>blazing fast</i> apps.
-          </Intro>
-        </Text>
-        <Button as={Link} to="/projects">
-          SHOW PROJECTS
-        </Button>
-      </Content>
-    </StyledMain>
+    <Home />
     <Technologies />
     <ProjectList />
     <Contact />

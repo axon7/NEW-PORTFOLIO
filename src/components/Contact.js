845d4ee805bbc9652c805dcd5f9aceff1d486855
Kuba <kubaz9991@gmail.com>
2020-01-19 14:49:41 +0100

    fix form 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -76,10 +76,22 @@ const Contact = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
-        <label>Name</label>
-        <ContactInput name="name" placeholder="Your Name" type="text" />
-        <label>e-mail</label>
-        <ContactInput name="email" placeholder="name@name.com" type="email" />
+        <p>
+          <label>
+            Name
+            <ContactInput name="name" placeholder="Your Name" type="text" />
+          </label>
+        </p>
+        <p>
+          <label>
+            E-mail
+            <ContactInput
+              name="email"
+              placeholder="name@name.com"
+              type="email"
+            />
+          </label>
+        </p>
         <ContactTextarea rows="8" name="message" required />
         <SubmitButton>SUBMIT</SubmitButton>{" "}
       </ContactForm>
